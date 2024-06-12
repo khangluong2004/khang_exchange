9d21e3bd7656af51376a9597bb04893338eaf46c
khangluong2004 <khangluong2004@gmail.com>
2024-06-12 11:41:55 +1000

    Add project files.

---
+++ khang_exchange.cpp
@@ -0,0 +1,12 @@
+﻿// khang_exchange.cpp : Defines the entry point for the application.
+//
+
+#include "khang_exchange.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
